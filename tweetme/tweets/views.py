61ef732868967ba7f13496c2d159f35a00a450e5
sharad4 <sharad011989@gmail.com>
2023-01-09 00:10:47 +0530

    view modified

--- tweetme/tweets/views.py
+++ tweetme/tweets/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
+
+from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
     return HttpResponse("<h1>Hello World</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+    """
+    REST API VIEW 
+    Consume by JavScript or Swift or Java or iOS/Android
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+    except:
+        data["message"] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
