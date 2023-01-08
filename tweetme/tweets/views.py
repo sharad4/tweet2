e1a2601fc9678a10c6278457f6b3767224108524
sharad4 <sharad011989@gmail.com>
2023-01-08 23:42:11 +0530

    routed urls

--- tweetme/tweets/views.py
+++ tweetme/tweets/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>Hello World</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
